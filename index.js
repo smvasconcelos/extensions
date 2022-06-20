baf195c61de45d9b183b56f421148f9545b53778
smvasconcelos <smvasconcelos11@gmail.com>
2022-06-20 01:05:15 -0300

    added server

---
+++ index.js
@@ -0,0 +1,55 @@
+const serviceAccountKey = require("./serviceAccountKey.json");
+const admin = require("firebase-admin");
+const fireApp = admin.initializeApp({
+	credential: admin.credential.cert(serviceAccountKey)
+});
+const firestore = admin.firestore(fireApp);
+
+const manhwaRef = firestore.collection('manhwa');
+const historyRef = firestore.collection('history');
+
+require('dotenv').config();
+if (process.env.ORIGIN === 'http://localhost:3000') {
+}
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", '*');
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+	next();
+});
+
+app.use(express.json())
+app.use(cors({ origin: "*" }))
+
+app.get('/add_manhwa', cors(), add_manhwa);
+
+async function add_manhwa(request, response) {
+	const data = request.data;
+	console.log({ data });
+	return "add manhwa"
+}
+
+app.get('/remove_manhwa', cors(), remove_manhwa);
+
+async function remove_manhwa(request, response) {
+	return "remove manhwa"
+}
+app.get('/add_history', cors(), add_history);
+
+async function add_history(request, response) {
+	return "add history"
+}
+app.get('/remove_history', cors(), remove_history);
+
+async function remove_history(request, response) {
+	return "remove_history"
+}
+
+app.listen(3002, () => {
+	console.log(`Example app listening on port ${3002}`)
+})
