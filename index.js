209e873bed3a7b8fe548530746b92ed06d2fb41e
smvasconcelos <smvasconcelos11@gmail.com>
2022-06-20 02:22:28 -0300

    .env heroku

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 require('dotenv').config();
-const admin = require("firebase-admin");
-const fireApp = admin.initializeApp({
-	credential: admin.credential.cert({
-		"type": process.env.FIREBASE_type,
-		"project_id": process.env.FIREBASE_project_id,
-		"private_key_id": process.env.FIREBASE_private_key_id,
-		"private_key": process.env.FIREBASE_private_key,
-		"client_id": process.env.FIREBASE_client_id,
-		"auth_uri": process.env.FIREBASE_auth_uri,
-		"token_uri": process.env.FIREBASE_token_uri,
-		"auth_provider_x509_cert_url": process.env.FIREBASE_auth_provider_x509_cert_url,
-		"client_x509_cert_url": process.env.FIREBASE_client_x509_cert_url,
-		"client_email": process.env.FIREBASE_client_email,
-	})
-});
-const firestore = admin.firestore(fireApp);
+// const admin = require("firebase-admin");
+// const fireApp = admin.initializeApp({
+// 	credential: admin.credential.cert({
+// 		"type": process.env.FIREBASE_type,
+// 		"project_id": process.env.FIREBASE_project_id,
+// 		"private_key_id": process.env.FIREBASE_private_key_id,
+// 		"private_key": process.env.FIREBASE_private_key,
+// 		"client_id": process.env.FIREBASE_client_id,
+// 		"auth_uri": process.env.FIREBASE_auth_uri,
+// 		"token_uri": process.env.FIREBASE_token_uri,
+// 		"auth_provider_x509_cert_url": process.env.FIREBASE_auth_provider_x509_cert_url,
+// 		"client_x509_cert_url": process.env.FIREBASE_client_x509_cert_url,
+// 		"client_email": process.env.FIREBASE_client_email,
+// 	})
+// });
+// const firestore = admin.firestore(fireApp);
 
-const manhwaRef = firestore.collection('manhwa');
-const historyRef = firestore.collection('history');
+// const manhwaRef = firestore.collection('manhwa');
+// const historyRef = firestore.collection('history');
 
 const express = require('express');
 const app = express();
