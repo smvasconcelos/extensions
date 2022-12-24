5f190acb8910d5f379f71b59e77b2a1320787314
Samuel Vasconcelos <smvasconcelos11@gmail.com>
2022-12-24 16:15:21 -0300

    Setting up react

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { PopupPage } from './pages/Popup/Popup.pages'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <PopupPage />
+  </React.StrictMode>,
+)
---
+++ src/pages/Popup/Popup.pages.tsx
@@ -0,0 +1,3 @@
+export function PopupPage(): JSX.Element {
+  return <>Olá</>
+}
