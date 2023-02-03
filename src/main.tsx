1acac66d7e082d369ff58ce9341871e86a620474
Samuel Vasconcelos <smvasconcelos11@gmail.com>
2023-02-03 15:15:12 -0300

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { GlobalStyles } from 'twin.macro';
 import { Home } from './pages/Home/Home.page';
 import { PopupPage } from './pages/Popup/Popup.page';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Router>
     <GlobalStyles />
     <Routes>
       <Route path="/" element={<PopupPage />} />
       <Route path="/home" element={<Home />} />
     </Routes>
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
