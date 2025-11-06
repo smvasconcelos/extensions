004fdb8a91ad8311e458570c1280ce05c93f7ec8
Samuel Vasconcelos <smvasconcelos11@gmail.com>
2025-11-05 21:19:32 -0300

    feat: add types module

---
+++ src/types/api.types.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { Manhwa, HistoryItem } from './manhwa.types';
+
+export interface ApiResponse<T> {
+	message: string;
+	status: number;
+	data?: T;
+	error?: unknown;
+}
+
+export interface RequestQuery {
+	email?: string;
+	url?: string;
+}
+
+export interface AddManhwaRequestBody {
+	url?: string;
+	chapter?: string;
+	name?: string;
+	email?: string;
+	img?: string;
+	card?: boolean;
+}
+
+export interface RemoveManhwaRequestBody {
+	data: Manhwa;
+	email: string;
+}
+
+export interface RemoveHistoryRequestBody {
+	data: HistoryItem;
+	email: string;
+}
+
+export interface TypedRequest<T = Record<string, never>> extends Request {
+	body: T;
+	query: RequestQuery;
+}
+
+export interface TypedResponse<T> extends Response {
+	send: (body: string | ApiResponse<T>) => Response;
+}
